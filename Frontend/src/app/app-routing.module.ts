dd6e701a407fc8397c2ba6889a52e8685a650bcc
Juan Sarmiento <jcsarmiento1@misena.edu.co>
2019-11-06 21:30:10 -0500

    Vista home creada

---
+++ Frontend/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'ingresar', loadChildren: './ingresar/ingresar.module#IngresarPageModule' },
+  { path: 'registrarse', loadChildren: './registrarse/registrarse.module#RegistrarsePageModule' },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
