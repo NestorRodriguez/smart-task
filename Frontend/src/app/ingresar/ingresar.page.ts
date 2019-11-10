d62a10637567262818b2b8d26e68fde1652bbe8a
Juan Sarmiento <jcsarmiento1@misena.edu.co>
2019-11-10 16:54:58 -0500

    mejoras del ingreso

--- Frontend/src/app/ingresar/ingresar.page.ts
+++ Frontend/src/app/ingresar/ingresar.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-ingresar',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ingresar.page.scss'],
 })
 export class IngresarPage implements OnInit {
-
+  
   constructor() { }
 
+  model: any = {};
   ngOnInit() {
+    this.model = {
+      correo : null,
+      password : null,
+    };
   }
-
+  public enviarData( formulario: NgForm ) {
+    if (formulario.valid) {
+       console.log(formulario);
+    }
+}
+public evento(evento:Event) {
+  console.log(evento);
+}
 }
