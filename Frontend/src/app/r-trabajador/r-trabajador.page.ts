e46ef81537ba3a168dbbb1a7fbb8b0605ad4e541
Néstor Andrés <nestorandres1@gmail.com>
2019-11-24 15:31:01 -0500

    Libreria Camera en app.module

--- Frontend/src/app/r-trabajador/r-trabajador.page.ts
+++ Frontend/src/app/r-trabajador/r-trabajador.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-// import {Camera, CameraOptions} from '@ionic-native/camera/ngx';
+import {Camera, CameraOptions} from '@ionic-native/camera/ngx';
 
 
 @Component({
@@ -13,8 +13,8 @@ export class RTrabajadorPage implements OnInit {
 
   model: any = {};
 
-  constructor() { }
-  // constructor(private camera: Camera) { }
+  //constructor() { }
+  constructor(private camera: Camera) { }
 
   ngOnInit() {
     this.model ={
@@ -33,15 +33,15 @@ public evento(evento:Event) {
   console.log(evento);
 }
 
-// async loadCamera() {
-//   const options = {
-//     quality: 25,
-//     destinationType: this.camera.DestinationType.DATA_URL,
-//     encodingType: this.camera.EncodingType.JPEG,
-//     mediaType: this.camera.MediaType.PICTURE,
-//     correctOrientation: true,
-//     cameraDirection: this.camera.Direction.BACK,
-//   };
-//   this.model.imagen = 'data:image/jpeg;base64,' + await this.camera.getPicture(options);
-// }
+async loadCamera() {
+  const options = {
+    quality: 25,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE,
+    correctOrientation: true,
+    cameraDirection: this.camera.Direction.BACK,
+  };
+  this.model.imagen = 'data:image/jpeg;base64,' + await this.camera.getPicture(options);
+}
 }
